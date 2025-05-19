df33c92d70e10367421dbcb5e2687be538fe1d1d
Jan Sher Khan <jansherkhan2239@gmail.com>
2025-05-19 22:40:34 +0500

    Add files via upload

---
+++ data_types_practice.py
@@ -0,0 +1,25 @@
+# Integer variables
+age = 23  # This is an integer representing age
+year = 2025  # This is an integer representing the current year
+
+# String variables
+name = "Jan Sher"  # This is a string representing a person's name
+city = "Peshawar"  # This is a string representing a city name
+
+# Float variables
+height = 5.9  # This is a float representing height in feet
+weight = 63.5  # This is a float representing weight in kilograms
+
+# Boolean variables
+is_student = True  # This is a boolean indicating if the person is a student
+has_passed = False  # This is a boolean indicating if the person has passed
+
+# Printing the variables with messages
+print("Age is:", age)
+print("Year is:", year)
+print("Name is:", name)
+print("City is:", city)
+print("Height is:", height)
+print("Weight is:", weight)
+print("Is student:", is_student)
+print("Has passed:", has_passed)
